883139e857057988d80357a6d8b7e88cdfcd19ed
hama1185 <hamataku0120@gmail.com>
2021-03-28 03:14:10 +0900

    センサーの値取得とandroidで実行できることを確認

--- Assets/Scripts/GetValue.cs
+++ Assets/Scripts/GetValue.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GetValue : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public TMP_Text ValueX;
+    public TMP_Text ValueY;
+    public TMP_Text ValueZ;
+    public TMP_Text ValueW;
+
+    Quaternion gyro;
+    
+    void Start(){
+        Input.gyro.enabled = true;    
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate(){
+        this.gyro = Input.gyro.attitude;
+        ValueX.text = this.gyro.x.ToString();
+        ValueY.text = this.gyro.y.ToString();
+        ValueZ.text = this.gyro.z.ToString();
+        ValueW.text = this.gyro.w.ToString();
     }
 }
